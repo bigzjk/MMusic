2ceced137f0330478b36f7cbc180724ce01fd11e
alkun <zhujunkun@yit.com>
2019-09-28 11:25:59 +0800

    fix: mmuisc

---
+++ src/pages/test/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import request from 'utils/request'
+import './index.scss'
+// url: 'cms_list_tag?nid=23831003&type=2006&pageNo=0&pageSize=10'
+// 'client_play_list_tag'
+class Test extends Component{
+    componentDidMount() {
+        
+
+    }
+    render() {
+        return(
+            <div className="Test">
+                dsadab
+            </div>
+        )
+    }
+}
+
+export default Test
